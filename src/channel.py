f25b8c05c4fbb2ff98b89d2dca0fb8d42d0955e4
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-09-13 19:06:24 +0500

    реализовала класс Channel

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,9 @@
+#import json
+import os
+from googleapiclient.discovery import build
+#import isodate
+
+from helper.youtube_api_manual import printj
 
 
 class Channel:
@@ -5,8 +11,14 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id=channel_id
 
-    def print_info(self) -> None:
+    def print_info(self ) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        api_key: str = os.environ['API__KEY']
+        youtube = build('youtube', 'v3', developerKey=api_key)
+        channel = youtube.channels().list(id=self.channel_id , part='snippet,statistics').execute()
+        print(printj(channel))
+
+obzbzbz=Channel('UC-OVMPlMA3-YCIeg4z5z23A')
+obzbzbz.print_info()

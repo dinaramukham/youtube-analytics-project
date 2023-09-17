148a886381838f86c557d46a561d987b0ecbc135
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-09-17 16:18:02 +0500

    дз 2

--- helper/youtube_api_manual.py
+++ helper/youtube_api_manual.py
@@ -13,7 +13,7 @@ api_key: str = os.environ['API__KEY']
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 
-def printj(dict_to_print: dict) -> None:
+def printj(dict_to_print: dict) -> None  :
     """Выводит словарь в json-подобном удобном формате с отступами"""
     print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
@@ -24,11 +24,10 @@ docs: https://developers.google.com/youtube/v3/docs/channels/list
 
 сервис для быстрого получения id канала: https://commentpicker.com/youtube-channel-id.php
 '''
-# channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
-channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+#channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
 channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
-printj(channel)
-
+printj(channel )
 
 '''
 получить данные по play-листам канала
@@ -38,7 +37,7 @@ playlists = youtube.playlists().list(channelId=channel_id,
                                      part='contentDetails,snippet',
                                      maxResults=50,
                                      ).execute()
-# printj(playlists)
+#printj(playlists)
 for playlist in playlists['items']:
     print(playlist)
     print()
--- src/channel.py
+++ src/channel.py
@@ -1,24 +1,34 @@
-#import json
+import json
 import os
 from googleapiclient.discovery import build
-#import isodate
-
-from helper.youtube_api_manual import printj
-
-
 class Channel:
     """Класс для ютуб-канала"""
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id=channel_id
+        self.channel_id = channel_id
+        self.title=self.print_info()["items"][0]["snippet"]["title"]
+        self.description=self.print_info()["items"][0]["snippet"]["description"]
+        self.url=self.print_info()["items"][0]["snippet"]["thumbnails"]["default"]["url"]
+        self.subscriber_count=self.print_info()["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.print_info()["items"][0]["statistics"]["videoCount"]
+        self.viewCount= self.print_info()["items"][0]["statistics"]["viewCount"]
+    def print_info(self) -> None:
+        """Выводит в консоль информацию о канале в формате словаря."""
+        youtube = Channel.get_service()
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        return channel
 
-    def print_info(self ) -> None:
-        """Выводит в консоль информацию о канале."""
-        api_key: str = os.environ['API__KEY']
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.channel_id , part='snippet,statistics').execute()
-        print(printj(channel))
-
-obzbzbz=Channel('UC-OVMPlMA3-YCIeg4z5z23A')
-obzbzbz.print_info()
+    def to_json(self, name_new_file):
+        with open(name_new_file, 'w') as f:
+            f.write(f"channel_id : {self.channel_id},\n"  # 
+                    f"title : {self.title},\n "
+                    f"description : {self.description},\n "
+                    f"url : {self.url}, \n"
+                    f"subscriber_count : {self.subscriber_count},\n "
+                    f"video_count : {self.video_count},\n "
+                    f"viewCount : {self.viewCount}. ")
+    @classmethod
+    def get_service(cls):
+        """ создать специальный объект для работы с API """
+        api_key: str = os.getenv('API__KEY')
+        return  build('youtube', 'v3', developerKey=api_key)

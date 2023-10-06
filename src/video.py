577db4f8c36ec014dacb12a9e3f5a99b02ae1777
Dinara Mukhametzianova <muxametmdm@gmail.com>
2023-10-06 16:22:11 +0500

    добавила исключения "неверное id videö"

--- src/video.py
+++ src/video.py
@@ -1,12 +1,20 @@
 from src.channel import  Channel
-from helper.youtube_api_manual import  printj
+#from helper.youtube_api_manual import  printj
 class Video:
     def __init__(self, video_id: str) -> None:
-        self.video_id=video_id    # id видео
-        self.title_video = self.print_info_video()["items"][0]["snippet"]["title"]     # название видео
-        self.url = self.print_info_video()["items"][0]["snippet"]["thumbnails"]["default"]["url"]   # ссылка на видео
-        self.views_count = self.print_info_video()["items"][0]["statistics"]["viewCount"] # количество просмотров
-        self.like_count = self.print_info_video()["items"][0]["statistics"]["likeCount"] # количество лайков
+        try:
+            self.video_id=video_id    # id видео
+            self.title = self.print_info_video()["items"][0]["snippet"]["title"]     # название видео
+            self.url = self.print_info_video()["items"][0]["snippet"]["thumbnails"]["default"]["url"]   # ссылка на видео
+            self.views_count = self.print_info_video()["items"][0]["statistics"]["viewCount"] # количество просмотров
+            self.like_count = self.print_info_video()["items"][0]["statistics"]["likeCount"] # количество лайков
+        except IndexError:
+            self.video_id = video_id  # id видео
+            self.title = None
+            self.url = None
+            self.views_count = None
+            self.like_count = None
+
     def __str__(self):
         return self.title_video
     def print_info_video(self) -> None:
@@ -20,7 +28,7 @@ class PLVideo(Video ):
         super().__init__(id_video )
         self.id_video=id_video
         self.id_playlist=id_playlist
-#a=Video('AWX4JnAnjBE')
-#print(a.url    )
+#a=Video('X4JnAnjBE') # AWX4JnAnjBE
+#print(a.title_video     )
 
 
